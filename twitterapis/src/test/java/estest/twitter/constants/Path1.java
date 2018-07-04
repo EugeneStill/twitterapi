02883fe223e056d7ff2dc68453cfcd57ff528ba2
Eugene <Eugene@10.0.0.70>
2018-07-04 13:42:00 -0700

    initial commit

---
+++ twitterapis/src/test/java/estest/twitter/constants/Path1.java
@@ -0,0 +1,6 @@
+package estest.twitter.constants;
+
+public class Path1 {
+	public static final String BASE_URI = "https://api.twitter.com";
+	public static final String STATUSES = "/1.1/statuses";
+}
